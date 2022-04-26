7facb41e6aa89b06e77607cd2e3c7c302fcea613
vadimmovlamov <vadim113512@tut.by>
2022-04-26 23:38:50 +0300

    add scss project

---
+++ Lesson2/src/index.js
@@ -0,0 +1,13 @@
+import './sass/main.scss';
+import './sass/title.scss';
+import './sass/fonts.scss';
+import './sass/header.scss';
+import './sass/intro.scss';
+import './sass/social.scss';
+import './sass/tabs.scss';
+import './sass/design.scss';
+import './sass/pricing.scss';
+import './sass/reviews.scss';
+import './sass/baner.scss';
+import './sass/contact.scss';
+import './sass/footer.scss';
--- Lesson2/src/js/script.js
+++ Lesson2/src/js/script.js
@@ -15,23 +15,21 @@ new Swiper('.reviews__block');
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
-  
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
-  
+});
