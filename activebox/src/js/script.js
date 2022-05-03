9428a92efa64288b3787d519d74cc40187427ae0
vadimmovlamov <vadim113512@tut.by>
2022-05-03 19:26:26 +0300

    add js-code and adaptive burger

--- activebox/src/js/script.js
+++ activebox/src/js/script.js
@@ -1,12 +1,3 @@
-/* burger */
-$(document).ready(function(){
-    $('.header__burger').click(function(event){
-        S('.header__burger,.header__menu').toggleClass('active');
-        S('body').toggleClass('lock');
-    });
-});
-
-
 /* scroll header */
 (function (){
     const header = document.querySelector('.header');
@@ -19,17 +10,65 @@ $(document).ready(function(){
     };
 }());
 
+/* burger*/
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
     });
     menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header__nav.active');
+        menu.classList.remove('header__nav_active');
     });
+    if(window.innerWidth <= 768){
+        for (let i = 0; i < menuLinks.length; i += 1){
+            menuLinks[i].addEventListener('click', () =>{
+                menu.classList.remove('header__nav_active');
+            });
+        }
+    }
 }());
+
+// Scroll to anchors
+(function () {
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
 /* swiper */
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
